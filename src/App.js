9923ca55b3c1de070fd2e88e127be1a53c5a9cbd
charutinho <alissoncf8@gmail.com>
2021-11-25 23:08:43 -0300

    estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
 
 function App() {
-  return <div>App Dogs</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <nav className={"container"}>
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+    </nav>
+  );
+};
+
+export default Header;
