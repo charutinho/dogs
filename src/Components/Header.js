caa7519d9b8616639f446b4a87c4326f78bc0366
charutinho <alissoncf8@gmail.com>
2021-11-26 21:36:36 -0300

    header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { ReactComponent as Dogs } from "../assets/dogs.svg";
 
 import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <nav className={"container"}>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
-    </nav>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link to="/" aria-label="Dogs | Home" className={styles.logo}>
+          <Dogs />
+        </Link>
+        <Link to="/login" className={styles.login}>
+          Login / Criar
+        </Link>
+      </nav>
+    </header>
   );
 };
 
