888b5464690230145a7c9c931ebf2f2be29f4a8f
charutinho <alissoncf8@gmail.com>
2022-03-01 03:15:30 -0300

    biblioteca de estatisticas

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import useFetch from "../../Hooks/useFetch";
 
 import Head from "../Helper/Head";
+import Loading from "../Helper/Loading";
+import Error from "../Helper/Error";
+
+import { STATS_GET } from "../../api";
+
+const UserStatsGraph = React.lazy(() => import("./UserStatsGraph"));
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatísticas" />
-      Estatísticas
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+
+  if (data)
+    return (
+      <React.Suspense fallback={<div></div>}>
+        <Head title="Estatísticas" />
+        <UserStatsGraph data={data} />
+      </React.Suspense>
+    );
+  else return null;
 };
 
 export default UserStats;
